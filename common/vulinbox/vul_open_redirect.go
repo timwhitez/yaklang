c27c616304fe50f0043ea96dc71ee04b78a9f529
rookie <rookie_is@qq.com>
2023-07-28 13:16:51 +0800

    feat:重构url重定向漏洞靶场

--- common/vulinbox/vul_open_redirect.go
+++ common/vulinbox/vul_open_redirect.go
@@ -7,61 +7,79 @@ import (
 )
 
 func (s *VulinServer) registerOpenRedirect() {
-	var router = s.router
-	router.HandleFunc("/redirect/safe", func(writer http.ResponseWriter, request *http.Request) {
-		var u = request.URL.Query().Get("url")
-		if u == "a" {
-			writer.Write([]byte("修改url参数跳转页面"))
-			writer.WriteHeader(200)
-			return
-		}
-		urlStruct, err := url.Parse(u)
-		if err != nil {
-			writer.Write([]byte(err.Error()))
-			writer.WriteHeader(500)
-			return
-		}
+	urlRedirectGroup := s.router.PathPrefix("/redirect").Name("URL重定向漏洞").Subrouter()
+	urlRedirectRoutes := []*VulInfo{
+		{
+			DefaultQuery: "url=a",
+			Path:         "/safe",
+			Title:        "安全的url重定向",
+			Handler: func(writer http.ResponseWriter, request *http.Request) {
+				var u = request.URL.Query().Get("url")
+				if u == "a" {
+					writer.Write([]byte("修改url参数跳转页面"))
+					writer.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+				urlStruct, err := url.Parse(u)
+				if err != nil {
+					writer.Write([]byte(err.Error()))
+					writer.WriteHeader(http.StatusInternalServerError)
+					return
+				}
 
-		if urlStruct.Host != request.Host {
-			writer.Write([]byte("不安全的重定向"))
-			writer.WriteHeader(500)
-			return
-		}
+				if urlStruct.Host != request.Host {
+					writer.Write([]byte("不安全的重定向"))
+					writer.WriteHeader(http.StatusInternalServerError)
+					return
+				}
 
-		writer.Header().Set("Location", u)
-		writer.WriteHeader(302)
-		return
-	})
+				writer.Header().Set("Location", u)
+				writer.WriteHeader(http.StatusFound)
+				return
+			},
+		},
+		{
+			DefaultQuery: "url=a",
+			Path:         "/include-white-list",
+			Title:        "只匹配关键字的url重定向",
+			Handler: func(writer http.ResponseWriter, request *http.Request) {
+				var u = request.URL.Query().Get("url")
+				if u == "a" {
+					writer.Write([]byte("修改url参数跳转页面"))
+					writer.WriteHeader(http.StatusOK)
+					return
+				}
 
-	router.HandleFunc("/redirect/include-white-list", func(writer http.ResponseWriter, request *http.Request) {
-		var u = request.URL.Query().Get("url")
-		if u == "a" {
-			writer.Write([]byte("修改url参数跳转页面"))
-			writer.WriteHeader(200)
-			return
-		}
+				if !strings.Contains(u, request.Host) {
+					writer.Write([]byte("不安全的重定向"))
+					writer.WriteHeader(http.StatusInternalServerError)
+					return
+				}
 
-		if !strings.Contains(u, request.Host) {
-			writer.Write([]byte("不安全的重定向"))
-			writer.WriteHeader(500)
-			return
-		}
+				writer.Header().Set("Location", u)
+				writer.WriteHeader(http.StatusFound)
+				return
+			},
+		},
+		{
+			DefaultQuery: "url=a",
+			Path:         "/no_protect",
+			Title:        "无保护的url重定向",
+			Handler: func(writer http.ResponseWriter, request *http.Request) {
+				var u = request.URL.Query().Get("url")
+				if u == "a" {
+					writer.Write([]byte("修改url参数跳转页面"))
+					writer.WriteHeader(http.StatusOK)
+					return
+				}
 
-		writer.Header().Set("Location", u)
-		writer.WriteHeader(302)
-		return
-	})
-
-	router.HandleFunc("/redirect/no_protect", func(writer http.ResponseWriter, request *http.Request) {
-		var u = request.URL.Query().Get("url")
-		if u == "a" {
-			writer.Write([]byte("修改url参数跳转页面"))
-			writer.WriteHeader(200)
-			return
-		}
-
-		writer.Header().Set("Location", u)
-		writer.WriteHeader(302)
-		return
-	})
+				writer.Header().Set("Location", u)
+				writer.WriteHeader(http.StatusFound)
+				return
+			},
+		},
+	}
+	for _, v := range urlRedirectRoutes {
+		addRouteWithVulInfo(urlRedirectGroup, v)
+	}
 }
